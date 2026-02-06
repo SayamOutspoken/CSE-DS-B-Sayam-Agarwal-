8d4042e95d4b6f775097cdaed83839e7a09153e0
Sayam Agarwal <sayamagarwal516@gmail.com>
2026-02-06 11:03:47 +0530

    5feb

--- 29th_jan/App.js
+++ 29th_jan/App.js
@@ -3,11 +3,15 @@ console.log("Hello");
 const parent = document.getElementById("container");
 const root = ReactDOM.createRoot(parent);
 
-const h2 = React.createElement("h2", {}, "Welcome to React dev");
-const li1 = React.createElement("li", {}, "Java Programming");
-const li2 = React.createElement("li", {}, "C++ Programming");
+root.render(React.createElement("h3", {}, "Loading..."));
 
-const ul = React.createElement("ul", {}, li1, li2);
-const div = React.createElement("div", {}, h2, ul);
+setTimeout(() => {
+    const h2 = React.createElement("h2", {}, "Welcome to React dev");
+    const li1 = React.createElement("li", {}, "Java Programming");
+    const li2 = React.createElement("li", {}, "C++ Programming");
 
-root.render(div);
+    const ul = React.createElement("ul", {}, li1, li2);
+    const div = React.createElement("div", {}, h2, ul);
+
+    root.render(div);
+}, 2000);
